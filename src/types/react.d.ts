ef5cce93e1ac79a6612bc4f7ebe3b451f11ddef1
yoonho <hunk9201@gmail.com>
2025-11-12 11:11:02 +0900

    github pages test

--- src/types/react.d.ts
+++ src/types/react.d.ts
@@ -2,7 +2,5 @@ import 'react';
 
 declare module 'react' {
   interface HTMLAttributes<T> extends AriaAttributes, DOMAttributes<T> {
-    // Add any additional HTML attributes you're using
-    [key: string]: any;
-  }
+    }
 }
